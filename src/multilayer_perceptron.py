e3f32afd07aa5009ae1bdbe8efc7037cec840202
victor <victoragcosta@hotmail.com>
2019-05-15 10:51:18 -0300

    Restructured HiddenLayer - Made code more uniform - Renamed parameters - Added docstring

--- src/multilayer_perceptron.py
+++ src/multilayer_perceptron.py
@@ -3,46 +3,71 @@ import theano
 import theano.tensor as T
 import matplotlib.pyplot as plt
 
+class HiddenLayer:
 
-class HiddenLayers:
+    def __init__(self, layer_input, n_in, n_out, random_generator, W=None, b=None, activation=T.tanh, name=None):
+        '''Cria uma camada escondida genérica
 
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.tanh):
+        :type layer_input: theano.tensor.TensorType
+        :param layer_input: variável que representa a entrada da camada
 
-        self.input = input
+        :type n_in: int
+        :param n_in: quantidade de entradas da camada
 
-        # initialize with random values the weights W as a matrix of shape (n_in, n_out)
+        :type n_out: int
+        :param n_out: quantidade de neurônios e saídas da camada
+
+        :type random_generator: numpy.random.RandomState
+        :param random_generator: um gerador de números aleatórios para criar
+                                 a matriz de pesos iniciais
+
+        :type activation: theano.Op or function
+        :param activation: função de ativação não linear
+
+        '''
+
+        self.layer_input = layer_input
+
+        # Initialize with random values the weights W as a matrix of shape (n_in, n_out)
         if W is None:
             W_values = np.asarray(
-                rng.uniform(
-                    low=-1,
-                    high=1,
+                random_generator.uniform(
+                    low=-6/(n_in+n_out),
+                    high=6/(n_in+n_out),
                     size=(n_in, n_out)
                 ),
                 dtype=theano.config.floatX
             )
 
-            W = theano.shared(value=W_values, name='W', borrow=True)
+            # Allocates weight matrix in theano
+            W = theano.shared(
+                value=W_values,
+                name='W' if name is None else 'W{}'.format(name),
+                borrow=True
+            )
 
-        # initialize the biases b as a vector of n_out 0s
+        # Initialize the biases b as a vector of n_out 0s
         if b is None:
+            b_values = np.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            )
+
+            # Allocates biases vector in theano
             b = theano.shared(
-                value=np.zeros(
-                    (n_out,),
-                    dtype=theano.config.floatX
-                ),
-                name='b',
+                value=b_values,
+                name='b' if name is None else 'b{}'.format(name),
                 borrow=True
             )
 
         self.W = W
         self.b = b
 
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (
-            lin_output if activation is None
-            else activation(lin_output)
-        )
-        # parameters of the model
+        # Tells theano how to calculate the linear output (before activation function)
+        lin_output = T.dot(layer_input, self.W) + self.b
+        # Tells theano how to calculate output (activation function applied
+        # on linear output)
+        self.output = lin_output if activation is None else activation(lin_output)
         self.params = [self.W, self.b]
 
 
